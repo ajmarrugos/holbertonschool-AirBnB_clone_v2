8652d10c672f2381d6e5d2a9ac8e4c2520d7280b
root <root@Holberton.localdomain>
2022-09-07 12:01:12 -0500

    Fixing User model

--- models/user.py
+++ models/user.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from os import getenv
-from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import Base, BaseModel
+
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a User with various attributes"""
     __tablename__ = "users"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete", passive_deletes=True)
+    reviews = relationship("Review", backref="user", cascade="all, delete", passive_deletes=True)
