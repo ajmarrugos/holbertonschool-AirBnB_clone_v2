711889eb133f7f441270ee378cc1490cda749c2b
root <root@Holberton.localdomain>
2022-09-07 12:32:25 -0500

    User object improvement

--- models/user.py
+++ models/user.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
 
-
 class User(BaseModel, Base):
     """This class defines a User with various attributes"""
+
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete", passive_deletes=True)
-    reviews = relationship("Review", backref="user", cascade="all, delete", passive_deletes=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
