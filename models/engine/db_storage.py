9905123a28fb3a5ca3929687f0b326d9c4b13d66
root <root@Holberton.localdomain>
2022-09-07 11:20:56 -0500

    Repair on the console

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ New Engine as DBStorage """
 from os import getenv
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.schema import MetaData
 
+engine =create_engine('')
 
 class DBStorage:
     """"DBStorage """
-__engine = None
-__session = None
+    __engine = None
+    __session = None
