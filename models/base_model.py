c94f16ebf78dabd3d778044783d4a92f6f8bd1e0
root <root@Holberton.localdomain>
2022-09-07 12:28:03 -0500

    Fixes on the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
-from datetime import datetime
 import models
+from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
