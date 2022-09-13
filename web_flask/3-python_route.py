dd83677dfb91e2c190cf96390207fd62b1291b34
ajmarrugos <ajmarrugos@gmail.com>
2022-09-13 12:45:29 -0500

    Added new route to app functions

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def index_02(text):
 @app.route('/python/<text>', strict_slashes=False)
 def index_03(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
-    
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
