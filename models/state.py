be529810a7e174170222ec0c18d7b2dd84df625f
ajmarrugos <ajmarrugos@gmail.com>
2022-09-13 20:58:20 -0500

    State class improvement: Enviroment usage + database connection

--- models/state.py
+++ models/state.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from os import getenv
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models import storage
 from models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 
 
 class State(BaseModel, Base):
     """State class"""
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='state', cascade="all, delete")
+    __tablename__ = "states"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """ Returns the list of City"""
+            the_cities = []
+            for c in models.storage.all(City).values():
+                if c.state_id == self.id:
+                    the_cities.append(c)
+            return the_cities
