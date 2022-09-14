b1be7020b2ee70d17212d736878ba38b7d7a0565
ajmarrugos <ajmarrugos@gmail.com>
2022-09-13 20:28:47 -0500

    Base Model upgrade: Enviroments + init method simplified

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,18 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 import models
+import datetime
+from os import getenv
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
+storageType = getenv("HBNB_TYPE_STORAGE")
+
+if storageType == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 Base = declarative_base()
@@ -19,24 +28,18 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        elif kwargs and kwargs.get('updated_at') is None:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+           
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            self.name = kwargs['name']
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if not self.id:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -63,4 +66,6 @@ class BaseModel:
         return dictionary
 
     def delete(self):
-        models.storage.delete(self)
+        """Deletes the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
