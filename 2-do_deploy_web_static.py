6b3ec3205fb3af58730a19ff40b10b5c39320291
ajmarrugos <ajmarrugos@gmail.com>
2022-09-14 23:44:15 -0500

    Deployment Process: Stage 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,11 +28,11 @@ def do_deploy(archive_path):
     try:
         put(archive_path, '/tmp/')
         file = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        no_ext = file.split(".")[0]
         path = "/data/web_static/releases/"
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file, path, no_ext))
+        run('rm /tmp/{}'.format(file))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
