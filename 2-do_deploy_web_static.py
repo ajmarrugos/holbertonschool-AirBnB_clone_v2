0dd757acb420fef1e2b9630d3c11f345a5264293
ajmarrugos <ajmarrugos@gmail.com>
2022-09-14 23:48:03 -0500

    Deployment Process: Stage 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,19 +26,17 @@ def do_deploy(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp/')
-        file = archive_path.split("/")[-1]
-        no_ext = file.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file, path, no_ext))
-        run('rm /tmp/{}'.format(file))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    except Exception as exc:
-        print(exc)
+    except:
         return False
