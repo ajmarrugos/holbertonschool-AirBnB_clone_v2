ae125bc77a9eeb0ace8b82c540b545a8cd55054e
ajmarrugos <ajmarrugos@gmail.com>
2022-09-14 23:23:54 -0500

    Deployment Process: Stage 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Deployment process: Stage 2"""
-import datetime
+import time
 from os import path
 from fabric.api import run, put, local, env
 
@@ -10,13 +10,14 @@ env.hosts = ['35.196.3.36', '204.236.195.39']
 def do_pack():
     """Returns the file path if has been correctly generated.
     Otherwise, returns none"""
-    local("mkdir -p versions")
-    date = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')
-    file = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                 .format(datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')))
-    if file.failed:
+    timest = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(timest))
+        return ("versions/web_static_{:s}.tgz".format(timest))
+    except Exception:
         return None
-    return ("versions/web_static_{}.tgz".format(date))
 
 
 def do_deploy(archive_path):
