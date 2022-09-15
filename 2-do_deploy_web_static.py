504f52c717e75a39eadd44303630f15dc90e1f00
ajmarrugos <ajmarrugos@gmail.com>
2022-09-14 23:39:50 -0500

    Deployment Process: Stage 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,18 +27,16 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, '/tmp/')
-
-        file = archive_path.split('/')[-1].split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(file))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'
-            .format(file, file))
-        run('rm /tmp/{}.tgz'.format(file))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}'.format(file, file))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(file))
+        file = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}\
-            /data/web_static/current'.format(file))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
 
     except Exception as exc:
